8bb004d27d7d0a61c48bd17644c09d1edf8be790
IvanSScrobot <ivanscrobot@gmail.com>
2025-11-26 20:31:53 -0800

    Lesson 20

---
+++ lesson_21/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! Version dsaf');
+});
+app.get('/health', (req, res) => {
+    res.send('Healthy');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
