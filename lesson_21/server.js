0e75fad51aa2541ddebdfd730b50b6d3c3eaac13
IvanSScrobot <ivanscrobot@gmail.com>
2025-12-01 20:19:16 -0800

    Lesson 22

--- lesson_21/server.js
+++ lesson_21/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +7,8 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello, World! Version dsaf');
 });
+
+
 app.get('/health', (req, res) => {
     res.send('Healthy');
 });
